f99836dca8529128954ab83db51745706c895d3c
GABRIEL ALVES POLETO <gabriel_poleto@estudante.sesisenai.org.br>
2024-11-04 12:14:42 -0300

    enviar

---
+++ Fomulario.js
@@ -0,0 +1,23 @@
+function Login(){
+   
+    // Dlecarar Variáveis
+    let Usuario = document.getElementById('Usuario').value
+    let Email = document.getElementById('Email').value
+    let Senha = document.getElementById('Senha').value
+    let CPF = document.getElementById('CPF').value
+    let DataDeNascimento = document.getElementById('DataDeNascimento').value
+
+    // Código do Botão
+    let sexo = document.querySelector("input[name='GENERO']:checked").value
+
+    // Alert
+    alert('Seu Usuario é: ' + Usuario + '\n' + 
+      'Seu email é: ' + Email + '\n' +
+      'Sua senha é: ' + Senha + '\n' +
+      'Seu cpf é:  ' + CPF + '\n' +
+      'Sua data de nascimento é: ' + DataDeNascimento + '\n' + 
+      'sexo: ' + sexo)
+
+    // Código da próxima página
+    z
+}
